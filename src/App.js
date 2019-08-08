bea0790b7dc8ec9950b1b62a0b7cb4e666858d64
hstrejoluna <170300130@ucaribe.edu.mx>
2019-08-08 13:15:56 -0500

    new

---
+++ src/App.js
@@ -0,0 +1,91 @@
+import React, { useState, useEffect } from 'react';
+
+import Header from './components/Header';
+import Formulario from './components/Formulario';
+import Error from './components/Error';
+import Clima from './components/Clima';
+
+function App() {
+
+  // State princial
+  // ciudad = state, guardarCiudad = this.setState()
+  const [ ciudad, guardarCiudad ] = useState('');
+  const [ pais, guardarPais] = useState('');
+  const [ error, guardarError ] = useState(false);
+  const [ resultado, guardarResultado] = useState({})
+  
+  useEffect(() => {
+  
+    // prevenir ejecución
+    if(ciudad === '') return;
+
+    const consultarAPI = async () => {
+      const appId = '27941e1c89d3864924c421f98c36b2ce';
+  
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`; 
+  
+      // consultar la URL
+      const respuesta = await fetch(url);
+      const resultado = await respuesta.json();
+
+      guardarResultado(resultado);
+    }
+
+    consultarAPI();
+  }, [ ciudad, pais ]);
+
+
+  const datosConsulta = datos => {
+      // Validar que ambos campos estén
+      if(datos.ciudad === '' || datos.pais === '') {
+        guardarError(true);
+        return;
+      }
+
+      // Ciudad y pais existen, agregarlos al state
+      guardarCiudad(datos.ciudad);
+      guardarPais(datos.pais);
+      guardarError(false);
+  }
+  
+  // Cargar un componente Condicionalmente
+  let componente;
+  if(error) {
+    // Hay un error, mostrarlo
+    componente = <Error mensaje='Ambos campos son obligatorios' />
+  } else if (resultado.cod === "404") {
+    componente = <Error mensaje="La ciudad no existe en nuestro registro" />
+  } else {
+    // Mostrar el Clima
+    componente = <Clima 
+                  resultado={resultado}
+                />;
+  }
+
+
+  return (
+    <div className="App">
+        <Header 
+          titulo='Clima React App'
+        />
+
+        <div className="contenedor-form">
+          <div className="container">
+            <div className="row">
+              <div className="col s12 m6">
+                  <Formulario 
+                    datosConsulta={datosConsulta}
+                  />
+              </div>
+
+              <div className="col s12 m6">
+                  {componente}
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+  );
+}
+
+export default App;
